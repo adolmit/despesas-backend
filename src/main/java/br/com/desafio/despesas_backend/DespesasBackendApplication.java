95d4c44739d636f28642b9ecc9f113fde27d7415
Adolfo Dario <adolmit@gmail.com>
2024-07-30 16:48:24 -0300

    feat: criação do projeto

---
+++ src/main/java/br/com/desafio/despesas_backend/DespesasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.despesas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasBackendApplication.class, args);
+	}
+
+}
